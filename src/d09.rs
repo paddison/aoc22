e4cd380dd8bfa762b05bd8bf4ebb21f7337e7b5c
paddison <patrickbaumann579@gmail.com>
2022-12-09 10:46:39 +0100

    update d09

--- src/d09.rs
+++ src/d09.rs
@@ -9,7 +9,7 @@ fn parse(input: &str) -> Vec<(Dir, usize)> {
 }
 
 
-fn update_head(dir: Dir, head: &mut(isize, isize)) {
+fn update_head(dir: Dir, head: &mut (isize, isize)) {
     match dir {
         Dir::Up => head.1 += 1,
         Dir::Right => head.0 += 1,
@@ -25,14 +25,10 @@ fn update_tail(tail: &mut (isize, isize), head: &(isize, isize)) {
         (-2, 0) => tail.0 -= 1,             // left
         (0, 2) => tail.1 += 1,              //up
         (0, -2) => tail.1 -= 1,             // down
-        (2, n) if n.is_positive() => { tail.0 += 1; tail.1 += 1; },     // up right
-        (2, n) if n.is_negative() => { tail.0 += 1; tail.1 -= 1; },     // down right
-        (-2, n) if n.is_positive() => { tail.0 -= 1; tail.1 += 1; },    // up left
-        (-2, n) if n.is_negative() => { tail.0 -= 1; tail.1 -= 1; },    // down left
-        (n, 2) if n.is_positive() => { tail.0 += 1; tail.1 += 1; },     // up right
-        (n, 2) if n.is_negative() => { tail.0 -= 1; tail.1 += 1; },     // up left
-        (n, -2) if n.is_positive() => { tail.0 += 1; tail.1 -= 1; },    // down right
-        (n, -2) if n.is_negative() => { tail.0 -= 1; tail.1 -= 1; },    // down left
+        (x, y) if x.is_positive() && y.is_positive() => { tail.0 += 1; tail.1 += 1; },     // up right
+        (x, y) if x.is_positive() && y.is_negative() => { tail.0 += 1; tail.1 -= 1; },     // down right
+        (x, y) if x.is_negative() && y.is_positive() => { tail.0 -= 1; tail.1 += 1; },    // up left
+        (x, y) if x.is_negative() && y.is_negative() => { tail.0 -= 1; tail.1 -= 1; },    // down left
         _ => unreachable!(),
     }
 }
@@ -57,7 +53,6 @@ fn execute_steps_p2(steps: Vec<(Dir, usize)>) -> usize {
     let mut visited = HashSet::<(isize, isize)>::new();
     let mut knots = [(0, 0); 10];
     for (dir, n_steps) in steps {
-        // println!("{:?}, {}",dir, n_steps);
 
         for _ in 0..n_steps {
             update_head(dir, &mut knots[0]);
@@ -65,7 +60,6 @@ fn execute_steps_p2(steps: Vec<(Dir, usize)>) -> usize {
                 let head = knots[i - 1];
                 update_tail(knots.get_mut(i).unwrap(), &head);
             }
-            // println!("{:?}", knots);
             visited.insert(knots[9].clone());
         }
     } 
@@ -101,41 +95,4 @@ pub fn get_solution_1() -> usize {
 
 pub fn get_solution_2() -> usize {
     execute_steps_p2(parse(include_str!("../data/d09.txt")))
-}
-
-fn test_data() -> Vec<(Dir, usize)> {
-    parse("R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2")
-}
-
-#[test]
-fn test_update_head() {
-    let mut head = (0, 0);
-    update_head(Dir::Up, &mut head);
-    assert_eq!((0, 1), head);
-}
-
-#[test]
-fn test_update_tail() {
-    let steps = test_data();
-    let n_visited = execute_steps(steps);
-    assert_eq!(n_visited, 13);
-}
-
-#[test]
-fn test_update_tail_p2() {
-    let steps = parse("R 5
-U 8
-L 8
-D 3
-R 17
-D 10
-L 25
-U 20");
-    let n_visited = execute_steps_p2(steps);
-    assert_eq!(n_visited, 36);
-}
-
-#[test]
-fn test_2() {
-    get_solution_2();
 }
