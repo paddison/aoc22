478ccf547761d551e2d83fa79102c27f2fb09a76
paddison <patrickbaumann579@gmail.com>
2022-12-12 16:24:15 +0100

    broken

--- src/d12.rs
+++ src/d12.rs
@@ -1,20 +1,20 @@
-use std::{collections::{HashMap, BinaryHeap}, fmt::Display};
+use std::{collections::{HashMap, BinaryHeap, VecDeque, HashSet}, fmt::Display};
 
 static INPUT: &str = include_str!("../data/d12.txt");
 static _TEST: &str = include_str!("../data/d12_test.txt");
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Graph {
     start: Node,
     goal: Node,
-    nodes: Vec<i8>,
+    heights: Vec<i8>,
     dim: (usize, usize) // row, col
 }
 
 impl Display for Graph {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for row in self.nodes.chunks(self.dim.1) {
-            let _ = writeln!(f, "{}", row.iter().map(|n| if *n != 35 { char::from_digit(*n as u32, 36).unwrap() } else { '.' }).collect::<String>());
+        for row in self.heights.chunks(self.dim.1) {
+            let _ = writeln!(f, "{}", row.iter().map(|n| if *n != 35 { char::from_digit(*n as u32, 36).unwrap_or('.') } else { '.' }).collect::<String>());
         }
 
         Ok(())
@@ -23,99 +23,111 @@ impl Display for Graph {
 
 impl Graph {
 
-    fn walk(&self) -> (Vec<(usize, usize)>, usize) {
-        let mut queue = BinaryHeap::new();
-        queue.push(self.start);
-        let mut costs = HashMap::from([(self.start.pos, 0_usize)]);
-        let mut path = HashMap::from([(self.start.pos, None)]);
- 
-        while !queue.is_empty() {
-            let cur =  queue.pop().unwrap();
-            if cur == self.goal {
-                return (Self::build_path(self.goal.pos, path), *costs.get(&cur.pos).unwrap());
-            }
-            for nb in self.get_neighbours(&cur) {
-                if !costs.contains_key(&nb.pos) {
-                    costs.insert(nb.pos, nb.cost);
-                    path.insert(nb.pos, Some(cur.pos));
-                    queue.push(nb);
-                }
-            }
-        }
-
-        (Vec::new(), 0)
+    fn dist(from: &(usize, usize), to: &(usize, usize)) -> usize {
+        from.0.abs_diff(to.0) + from.1.abs_diff(to.1) 
     }
 
-    fn hike(&mut self) -> usize {
-        let starting_positions = self.nodes.iter().enumerate().filter(|(_, n)| **n <= 1).map(|(idx, _)| self.rev_idx(idx)).collect::<Vec<(usize, usize)>>();
-        let mut paths = Vec::new();
-        for pos in starting_positions {
-            let mut new_start = Node { val: 0, pos, dist: 0, cost: 0 };
-            new_start.dist = self.dist(&new_start);
-            self.start = new_start;
-            paths.push(self.walk().1);
-        }
-        *paths.iter().filter(|n| **n != 0).min().unwrap()
+    fn idx(&self, row: usize, col: usize) -> usize {
+        self.dim.1 * row + col
     }
 
-    fn build_path(goal: (usize, usize), path: HashMap::<(usize, usize), Option<(usize, usize)>>) -> Vec<(usize, usize)> {
-        let mut reconstructed_path = vec![goal];
-        let mut cur = goal;
-        while let Some(Some(n)) = path.get(&cur) {
-            reconstructed_path.push(*n);
-            cur = *n;
-        }
-
-        reconstructed_path
-    }
-
-    fn rev_idx(&self, idx: usize) -> (usize, usize) {
+    fn coords(&self, idx: usize) -> (usize, usize) {
         (idx / self.dim.1, idx % self.dim.1)
     }
 
-    fn dist(&self, n: &Node) -> usize {
-        n.pos.0.abs_diff(self.goal.pos.0) + n.pos.1.abs_diff(self.goal.pos.1)
-    }
-
-    fn get_mut(&self, row: usize, col: usize) -> Option<i8> {
+    fn height(&self, row: usize, col: usize) -> Option<i8> {
         if row >= self.dim.0 || col >= self.dim.1 {
-            None
-        } else {
-            let idx = self.idx(row, col);
-            Some(self.nodes[idx])
-        }
+            return None;
+        } 
+        Some(self.heights[self.idx(row, col)])
     }
 
     // return only neighbours which are same, or one level higher
     fn get_neighbours(&self, node: &Node) -> Vec<Node> {
         let (row, col) = node.pos;
         let mut nb = Vec::new();
+        let neighbours_pos = [(row, col.overflowing_sub(1).0), (row, col + 1), (row.overflowing_sub(1).0, col), (row + 1, col)];
 
-        let others_pos = [(row, col.overflowing_sub(1).0), (row, col + 1), (row.overflowing_sub(1).0, col), (row + 1, col)];
-
-        for (r, c) in others_pos {
-            if let Some(other) = self.get_mut(r, c) {
-                if other - node.val < 2 {
-                    let n = Node { val: other, pos: (r, c), dist: self.dist(&self.goal), cost: node.cost + 1 };
+        for (r, c) in neighbours_pos {
+            if let Some(height) = self.height(r, c) {
+                if height - node.height < 2 {
+                    let n = Node { height, pos: (r, c), dist: Self::dist(&(r, c), &self.goal.pos), cost: node.cost + 1 };
                     nb.push(n);
                 }
             }
         }
-    
         nb
     }
 
-    fn idx(&self, row: usize, col: usize) -> usize {
-        self.dim.1 * row + col
+    fn walk(&mut self) -> Option<usize> {
+        let mut queue = BinaryHeap::new();
+        queue.push(self.start);
+ 
+        while !queue.is_empty() {
+            println!("{}", self);   
+            let cur = queue.pop().unwrap();
+            for nb in self.get_neighbours(&cur) {
+                if nb == self.goal {
+                    return Some(cur.cost);
+                }
+                let idx = self.idx(nb.pos.0, nb.pos.1);
+                self.heights[idx] = i8::MAX;
+                queue.push(nb);
+            }
+        }
+
+        None
+    }
+
+    fn hike(g: Self) -> usize {
+        let starting_positions = g.heights.iter()
+                                        .enumerate()
+                                        .filter(|(_, n)| **n <= 1)
+                                        .map(|(idx, _)| g.coords(idx))
+                                        .collect::<Vec<(usize, usize)>>();
+        let mut paths = Vec::new();
+        let n_threads = 4;
+        let chunk_size = starting_positions.len() / n_threads;
+        let mut handles = Vec::new();
+        
+        for id in 0..n_threads {
+            let g_local = g.clone();
+
+            let mut slice = Vec::new();
+
+            for pos in &starting_positions[id * chunk_size..id * chunk_size + chunk_size] {
+                slice.push(*pos);
+            }
+
+            let handle = std::thread::spawn(move || {
+                let mut paths = Vec::new();
+                let mut g_local_1 = g_local.clone();
+                for pos in slice {
+                    g_local_1.start = Node { height: 0, pos, dist: Self::dist(&pos, &g_local_1.goal.pos), cost: 0 };
+                    if let Some(steps) = g_local_1.walk() {
+                        paths.push(steps)
+                    }
+                }
+                paths
+            });
+
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            paths.push(handle.join().map(|v| *v.iter().min().unwrap_or(&usize::MAX)).unwrap());
+        }
+
+        *paths.iter().min().unwrap()
     }
 }
 
 #[derive(Eq, Hash, Clone, Copy, Debug)]
 struct Node {
-    val: i8,
-    pos: (usize, usize), // row, col
+    height: i8,
     dist: usize,
     cost: usize,
+    pos: (usize, usize), // row, col
 }
 
 impl PartialEq for Node {
@@ -140,8 +152,9 @@ fn parse(input: &str) -> Graph {
     let mut nodes = Vec::new();
     let cols = input.find('\n').unwrap();
     let rows = input.len() / cols;
-    let mut start = Node { val: 0, pos: (0, 0), dist: 0, cost: 0};
-    let mut goal = Node { val: 27, pos: (0, 0), dist: 0, cost: usize::MAX };
+    let mut start = Node { height: 0, pos: (0, 0), dist: 0, cost: 0};
+    let mut goal = Node { height: 27, pos: (0, 0), dist: 0, cost: usize::MAX };
+
     for (row, line) in input.lines().enumerate() {
         for (col, c) in line.chars().enumerate() {
             match c {
@@ -151,36 +164,23 @@ fn parse(input: &str) -> Graph {
             }
         }
     }
-    start.dist = start.pos.0.abs_diff(goal.pos.0) + start.pos.1.abs_diff(goal.pos.1);
-    Graph { start, goal, nodes, dim: (rows, cols) }
+
+    start.dist = Graph::dist(&start.pos, &goal.pos);
+    Graph { start, goal, heights: nodes, dim: (rows, cols) }
 }
 
 pub fn get_solution_1() -> usize {
-    let g = parse(INPUT);
-    g.walk().1 
+    let mut g = parse(INPUT);
+    g.walk().unwrap()
 }
 
 pub fn get_solution_2() -> usize {
-    let mut g = parse(INPUT);
-    g.hike()
-}
-
-
-#[test]
-fn test_do_digit() {
-    println!("{}", 'z'.to_digit(36).unwrap() - 10);
+    let g = parse(INPUT);
+    Graph::hike(g)
 }
 
 #[test]
-fn test_walk() {
+fn test() {
     let mut g = parse(INPUT);
-    let (path, c) = g.walk();
-    println!("{}", path.len());
-    for (row, col) in path {
-        let idx = g.idx(row, col);
-        let n = &mut g.nodes[idx];
-        *n = 35;
-    }
-    println!("{}", g);
-    println!("{}", c);
+    g.walk().unwrap(); 
 }
